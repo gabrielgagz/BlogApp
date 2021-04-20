23f16765dd4b1b2166fd18ccb45f9cf978de45f4
gabrielgagz <g.a.gomez@gmail.com>
2021-04-20 10:56:06 -0300

    SERVER: add dotenv and set up database connection
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- server/helpers/sequelize.js
+++ server/helpers/sequelize.js
@@ -1,4 +1,10 @@
 'use-strict'
 
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
+//Connect to DB
 const sequelize = new Sequelize(`postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/blog`);
+
+
+module.exports= sequelize;
