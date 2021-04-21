561fe3faa6f6a6269fdb16f3ca0bc61ed8f74729
gabrielgagz <g.a.gomez@gmail.com>
2021-04-21 19:44:55 -0300

    CLIENT: add skeleton
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MainScreen } from './components/MainScreen';
+import './css/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainScreen />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
