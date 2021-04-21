cc9ff97900bac00071aeb8c25be8ae00264a0bfe
gabrielgagz <g.a.gomez@gmail.com>
2021-04-21 20:20:51 -0300

    CLIENT: add basic routing
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { MainScreen } from '../components/MainScreen';
 
 export const AppRouter = () => {
+
     return (
-        <>
-            
-        </>
+        <Router>
+            <>
+                <Switch>
+                    <Route exact path='/blog' component={ MainScreen } />
+                    <Route exact path='/' component={ MainScreen } />
+                    <Redirect to='/' />
+                </Switch>
+            </>
+        </Router>
     )
 }
