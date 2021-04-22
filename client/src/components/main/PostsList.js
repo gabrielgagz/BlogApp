230dcc637a0ac53fc9aae1fd401250db3209bd0a
gabrielgagz <g.a.gomez@gmail.com>
2021-04-21 23:10:59 -0300

    CLIENT: set up post list grid
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/main/PostsList.js
+++ client/src/components/main/PostsList.js
@@ -1,42 +1,55 @@
-import React from 'react';
+import React from "react";
+import "../../css/postlist.css";
 
 export const PostsList = () => {
     return (
-        <div className='container-fluid'>
-        <div className="row row-cols-1 row-cols-md-2 g-4 mt-5">
-            <div className="col">
-                <div className="card m-4 shadow border-0">
-                    <img
-                        src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
-                        className="card-img-top"
-                        alt="..."
-                    />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            This is a longer card with supporting text below as
-                            a natural lead-in to additional content. This
-                            content is a little bit longer.
-                        </p>
+        <div className="container-fluid px-5">
+            <div className="row row-cols-1 row-cols-md-3 my-5 justify-content-center">
+                <div className="col">
+                    <div className="card m-2 shadow border-0 animate__animated animate__fadeIn">
+                        <img
+                            src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <div className="card-category border border-warning border-2 rounded text-warning text-center py-1 px-3">
+                                <small>CATEGORY</small>
+                            </div>
+                            <h5 className="card-title mt-4 my-3">
+                                [ Card title, a long card title ]
+                            </h5>
+                            <p>
+                                <span className="fw-light fst-italic text-muted">
+                                    <small>By <span className="text-success">Author</span> - 2021-04-21</small>
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="card m-4 shadow border-0">
-                    <img
-                        src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
-                        className="card-img-top"
-                        alt="..."
-                    />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            This is a longer card with supporting text below as
-                            a natural lead-in to additional content. This
-                            content is a little bit longer.
-                        </p>
+                <div className="col">
+                    <div className="card m-2 shadow border-0 animate__animated animate__fadeIn">
+                        <img
+                            src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <div className="card-category border border-warning border-2 rounded text-warning text-center py-1 px-3">
+                                <small>CATEGORY</small>
+                            </div>
+                            <h5 className="card-title mt-4 my-3">
+                                [ Card title, a long card title ]
+                            </h5>
+                            <p>
+                                <span className="fw-light fst-italic text-muted">
+                                    <small>By <span className="text-success">Author</span> - 2021-04-21</small>
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
     );
-}
+};
