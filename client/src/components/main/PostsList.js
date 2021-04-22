6db647952458955c72922bac530e153a7ab835a0
gabrielgagz <g.a.gomez@gmail.com>
2021-04-22 02:52:16 -0300

    CLIENT: improve post list. Use env vars for api key helper
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/main/PostsList.js
+++ client/src/components/main/PostsList.js
@@ -1,54 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getPosts } from '../../helpers/apiHelper';
 import "../../css/postlist.css";
 
 export const PostsList = () => {
+
+    // Post list state
+    const [ dataState, setDataState ] = useState();
+
+    // Effect to load post at init 
+    // TODO: use context to reload content
+    useEffect(() => {
+
+        // Get all posts
+        getPosts()
+            .then(data => setDataState( data ))
+            .catch(reason => console.log(reason.message));
+        
+    }, []);
+
     return (
         <div className="container-fluid px-5">
             <div className="row row-cols-1 row-cols-md-3 my-5 justify-content-center">
-                <div className="col">
-                    <div className="card m-2 shadow border-0 animate__animated animate__fadeIn">
-                        <img
-                            src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
-                            className="card-img-top"
-                            alt="..."
-                        />
-                        <div className="card-body">
-                            <div className="card-category border border-warning border-2 rounded text-warning text-center py-1 px-3">
-                                <small>CATEGORY</small>
+                {
+                
+                ( dataState ) &&
+                
+                // Map the results and print a card per item
+                dataState.map( ( { id, category, date,  image, title } ) => {
+
+                    // Choose a random color for category label
+                    const setCategoryColor = () => {
+
+                        return ['info', 'warning', 'primary', 'secondary'].sort(() => Math.random() - Math.random()).slice(0,1);
+                
+                    }
+
+                    const categoryColor = setCategoryColor();
+                
+                    return (
+                        <div className="col" key={ id }>
+                            <div className="card m-3 shadow border-0 animate__animated animate__fadeIn">
+                                <img
+                                    src={ image }
+                                    className="card-img-top"
+                                    alt="..."
+                                />
+                                <div className="card-body">
+                                    <div className={`card-category border border-${ categoryColor } border-2 rounded text-${ categoryColor } text-center py-1 px-3`}>
+                                        <small>{ category }</small>
+                                    </div>
+                                    <h5 className="card-title mt-4 my-3">
+                                        [ { title } ]
+                                    </h5>
+                                    <p>
+                                        <span className="fw-light fst-italic text-muted">
+                                            <small>By <span className="text-success">Author</span> - {  date.toString().substr(0,10) }</small>
+                                        </span>
+                                    </p>
+                                </div>
                             </div>
-                            <h5 className="card-title mt-4 my-3">
-                                [ Card title, a long card title ]
-                            </h5>
-                            <p>
-                                <span className="fw-light fst-italic text-muted">
-                                    <small>By <span className="text-success">Author</span> - 2021-04-21</small>
-                                </span>
-                            </p>
                         </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card m-2 shadow border-0 animate__animated animate__fadeIn">
-                        <img
-                            src="https://images.pexels.com/photos/5476235/pexels-photo-5476235.jpeg"
-                            className="card-img-top"
-                            alt="..."
-                        />
-                        <div className="card-body">
-                            <div className="card-category border border-warning border-2 rounded text-warning text-center py-1 px-3">
-                                <small>CATEGORY</small>
-                            </div>
-                            <h5 className="card-title mt-4 my-3">
-                                [ Card title, a long card title ]
-                            </h5>
-                            <p>
-                                <span className="fw-light fst-italic text-muted">
-                                    <small>By <span className="text-success">Author</span> - 2021-04-21</small>
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                    );
+                })
+
+                }
             </div>
         </div>
     );
--- client/src/helpers/apiHelper.js
+++ client/src/helpers/apiHelper.js
@@ -1,3 +1,6 @@
-export const handleApi = () => {
-    return
+export const getPosts = async () => {
+
+    const response = await fetch(`${ process.env.REACT_APP_API_KEY }/posts`);
+    return await response.json();
+
 }
