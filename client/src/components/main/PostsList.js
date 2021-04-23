bdc140d7f4dacd90e21be617d5b50b1d64e70f29
gabrielgagz <g.a.gomez@gmail.com>
2021-04-23 06:40:53 -0300

    CLIENT: add key per item on posts list
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/main/PostsList.js
+++ client/src/components/main/PostsList.js
@@ -37,12 +37,12 @@ export const PostsList = () => {
                     const categoryColor = setCategoryColor();
                 
                     return (
-                        <div className="col">
+                        <div className="col" key={ data.id }>
                             <div className="card m-3 shadow border-0 animate__animated animate__fadeIn">
                                 <img
                                     src={ data.image }
                                     className="card-img-top"
-                                    alt="..."
+                                    alt={ data.category.name }
                                 />
                                 <div className="card-body">
                                     <div className={`card-category border border-${ categoryColor } border-2 rounded text-${ categoryColor } text-center py-1 px-3`}>
@@ -54,10 +54,10 @@ export const PostsList = () => {
                                     <p>
                                         <span className="span-footer fw-light fst-italic text-muted">
                                             <small>By <span className="text-success">Author</span> - {  data.date.toString().substr(0,10) }</small>
-                                            <small>
-                                                <i className="far fa-edit mx-2"></i>
-                                                <i className="far fa-trash-alt mx-2"></i>
-                                            </small>
+                                                <small>
+                                                    <i className="far fa-edit mx-2"></i>
+                                                    <i className="far fa-trash-alt mx-2"></i>
+                                                </small>
                                         </span>
                                     </p>
                                 </div>
