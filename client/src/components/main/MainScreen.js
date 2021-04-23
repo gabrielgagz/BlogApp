83e089b5415447aa559019c2b02dd8ac5b401903
gabrielgagz <g.a.gomez@gmail.com>
2021-04-23 06:53:51 -0300

    CLIENT: put Main in context
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/main/MainScreen.js
+++ client/src/components/main/MainScreen.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBar } from '../ui/NavBar';
 import { PostsList } from './PostsList';
+import { AppContext } from '../../context/AppContext';
 
 export const MainScreen = () => {
+
+    const [reload, setReload] = useState( false );
+
     return (
-        <>
+            <>
             <NavBar />
-            <PostsList />
-        </>
+            <AppContext.Provider value={{ reload, setReload }}>
+                <PostsList />
+            </AppContext.Provider>
+            </>
     )
 }
